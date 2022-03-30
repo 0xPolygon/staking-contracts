90257b0ef518f9c4e5ebd09bd6999a842b1dc26f
AleksaOpacic <aleksaopacic988@gmail.com>
2022-03-30 12:39:03 +0200

    Merge staking contract test in one file

--- test/Staking.ts
+++ test/Staking.ts
@@ -5,7 +5,16 @@ import { expect } from "chai";
 import { Staking } from "../types/Staking";
 
 
-describe("Staking contract", function () {
+describe("Staking contract deployment", async () => {
+  it("Min validators should not be greater than max validators number", async function () {
+    const contractFactory = await ethers.getContractFactory("Staking");
+    await expect(contractFactory.deploy(7, 6)).to.be.revertedWith(
+        "Min validators num can not be greater than max num of validators"
+    );
+  })
+})
+
+describe("Staking and Unstaking", function () {
   let accounts: SignerWithAddress[];
   let contract: Staking;
   beforeEach(async () => {
